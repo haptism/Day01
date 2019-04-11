e80397b7dabe901f009c4cccd9db875dbe4226ac
{haptism} <haptism007@gmail.com>
2019-04-11 18:19:15 +0900

    k
    k

--- src/Day_01_4.java
+++ src/Day_01_4.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
 
 public class Day_01_4 {
+
 	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
 		System.out.println(sc);
 
-		/*
-		 * System.out.println("이름을 입력하세요"); String name = sc.next();
-		 * System.out.println("나이를 입력하세요"); int age = sc.nextInt();
-		 * System.out.println("your Name : " + name); System.out.println("your Age : " +
-		 * age);
-		 */
+//		System.out.println("이름을 입력하세요");
+//		String name = sc.next();
+//		System.out.println("나이를 입력하세요");
+//		int age = sc.nextInt();
+//		System.out.println("your Name : " + name);
+//		System.out.println("your Age : " + age);
 
 		// 희망 월급을 입력 받기 3000000
 		// 국민연금 : 0.3%
@@ -21,13 +23,15 @@ public class Day_01_4 {
 		// 각 세금의 금액
 		// 총 세금의 합계액
 		// 실 수령액
+
 		System.out.println("희망 월급을 입력하세요");
 		int sal = sc.nextInt();
+		
 		double taxnat = sal * 0.003;
 		double taxemp = sal * 0.005;
 		double taxmed = sal * 0.02;
 		double taxindu = sal * 0.012;
-		double total =taxnat+taxemp+taxmed+taxindu;
+		double total = taxnat + taxemp + taxmed + taxindu;
 		double myPay = total;
 
 		System.out.println("국민 연금 : " + taxnat + "원");
--- src/Day_01_5.java
+++ src/Day_01_5.java
@@ -3,12 +3,15 @@ import java.util.Scanner;
 public class Day_01_5 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.println(sc);
-		/*
-		 * int n1 = 10; int n2 = 3; int r1 = n1 / n2; int r2 = n1 % n2;
-		 * System.out.println(r1); System.out.println(r2);
-		 */
+
+		int n1 = 10;
+		int n2 = 3;
+		int r1 = n1 / n2;
+		int r2 = n1 % n2;
+		System.out.println(r1);
+		System.out.println(r2);
 
 		// 편의점 알바
 		// 물건의 합계 금액 입력 : 25640
@@ -19,17 +22,26 @@ public class Day_01_5 {
 		// 백원 3
 		// 십원 6
 
-		/*
-		 * int pay = 50000; System.out.println("손님이 낼 금액을 입력하세요 : " + pay + "원"); int
-		 * total = 25640; System.out.println("물건의 합계 금액을 입력하세요 : " + total + "원"); int
-		 * change = pay - total; System.out.println("거스름돈 : " + change + "원"); int man =
-		 * change / 10000; int ocheon = change / 5000; int cheon = change / 1000; int
-		 * obeak = change / 500; int beak = change / 100; int sib = change / 10;
-		 * 
-		 * System.out.println("만원 : " + man); System.out.println("오천원 : " + ocheon);
-		 * System.out.println("천원 : " + cheon); System.out.println("오백원 : " + obeak);
-		 * System.out.println("백원 : " + beak); System.out.println("십원 : " + sib);
-		 */
+		int pay = 50000;
+		System.out.println("손님이 낼 금액을 입력하세요 : " + pay + "원");
+		int total = 25640;
+		System.out.println("물건의 합계 금액을 입력하세요 : " + total + "원");
+		int change = pay - total;
+		System.out.println("거스름돈 : " + change + "원");
+
+		int man = change / 10000;
+		int ocheon = change / 5000;
+		int cheon = change / 1000;
+		int obeak = change / 500;
+		int beak = change / 100;
+		int sib = change / 10;
+
+		System.out.println("만원 : " + man);
+		System.out.println("오천원 : " + ocheon);
+		System.out.println("천원 : " + cheon);
+		System.out.println("오백원 : " + obeak);
+		System.out.println("백원 : " + beak);
+		System.out.println("십원 : " + sib);
 
 		// 4의 배수마다 +7 나머지 +1
 		// 951224-1234567
@@ -50,11 +62,6 @@ public class Day_01_5 {
 
 		// 12 -> 30
 
-		/*
-		 * int price = 25640; int don = 50000; int jandon = don - price; int man =
-		 * jandon / 10000; int chon = jandon % 10000 / 1000; int vack = jandon % 1000 /
-		 * 100; int sib = jandon % 100 / 10;
-		 */
 
 		// num / 4 * 10 + num % 4
 
